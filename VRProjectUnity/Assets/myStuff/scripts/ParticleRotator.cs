60642289c2fb99fb3c4b8246f63567d3b7a3aac8
N0733554 <43959754+N0733554@users.noreply.github.com>
2020-03-06 18:30:08 +0000

    Wand Added
    Basis of teh wand script has been added with some basic particle effects on charging and charged

---
+++ VRProjectUnity/Assets/myStuff/scripts/ParticleRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRotator : MonoBehaviour
+{
+    public GameObject Object;
+    
+    public float RotationSpeed;
+
+    void Update()
+    {
+        Object.transform.Rotate(Vector3.forward, RotationSpeed * Time.deltaTime);
+    }
+}
---
+++ VRProjectUnity/Assets/myStuff/scripts/Wand.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wand : MonoBehaviour
+{
+    public GameObject wandHeadPoint;
+
+    private Transform wandHeadTransform;
+    private Vector3 headPos;
+    private Vector3 prevHeadPos;
+
+    float wandSpeed = 0;
+
+    float wandCharge = 0;
+    float maxCharge = 100;
+
+    bool isHeld = false;
+    bool isMoving = false;
+    bool isCharging = false;
+    bool isCharged = false;
+
+    public ParticleSystem[] chargingSystems;
+    public ParticleSystem[] chargedSystems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopParticles(chargingSystems);
+        stopParticles(chargedSystems);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wandHeadTransform = wandHeadPoint.transform;
+        headPos = transform.TransformPoint(wandHeadTransform.position);
+
+        wandSpeed = Vector3.Distance(headPos, prevHeadPos) / Time.deltaTime;
+
+        isMoving = false;
+        if (wandSpeed > 0)
+            isMoving = true;
+        
+        if(isMoving && isHeld)
+        {
+            if(isCharging)
+                increaseWandCharge();
+        }
+
+        prevHeadPos = headPos;
+    }
+
+    void increaseWandCharge()
+    {
+        int chargeAmount = (int)Mathf.Floor(wandSpeed)/10;
+        wandCharge += chargeAmount;
+
+        if (wandCharge >= maxCharge)
+            wandCharged();
+    }
+
+    void wandCharged()
+    {
+        isCharged = true;
+        isCharging = false;
+        stopParticles(chargingSystems);
+        playParticles(chargedSystems);
+    }
+
+    void playParticles(ParticleSystem[] particleSystems)
+    {
+        if(particleSystems.Length > 0)
+        {
+            foreach (ParticleSystem ps in particleSystems)
+            {
+                ps.Play();
+            }
+        }        
+    }
+
+    void stopParticles(ParticleSystem[] particleSystems)
+    {
+        if (particleSystems.Length > 0)
+        {
+            foreach (ParticleSystem ps in particleSystems)
+            {
+                ps.Stop();
+            }
+        }
+    }
+
+    public void startHolding()
+    {
+        isHeld = true;
+        
+        if(isCharged)
+        {
+            playParticles(chargedSystems);
+        }
+        else
+        {
+            isCharging = true;
+            playParticles(chargingSystems);
+        }
+
+    }
+
+    public void stopHolding()
+    {
+        isHeld = false;
+
+        if (isCharged)
+        {
+            stopParticles(chargedSystems);
+        }
+        else
+        {
+            isCharging = false;
+            stopParticles(chargingSystems);
+        }
+    }
+
+    
+}
